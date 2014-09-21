fed0ea74261cc461cfc902e2345d0dc8c85c1222
imlonghao <imlonghao@gmail.com>
2014-09-21 13:00:34 +0800

    Optimizate Referer , timeout and port setting.

--- httpscan.py
+++ httpscan.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#多端口请求http
+# 多端口请求http
+# python httpscan.py example.com timeout port-from port-to (referer-url)
 __author__ = 'kttzd'
 
 import sys
@@ -8,28 +9,32 @@ import httplib2
 
 headers={}
 headers['User-Agent']="Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6"
-#headers['Referer']=""  #如果需要Referer从这里面加上
 
 address=sys.argv[1]
-
-ports=map(str,range(80,3000)) #默认80-2999 自己修改
+times=int(sys.argv[2])
+ports=map(str,range(int(sys.argv[3]),int(sys.argv[4])))
+try:
+    headers['Referer']=sys.argv[5]
+except:
+    pass
 
 class httpscan():
-    def __init__(self,address,ports,headers):
+    def __init__(self,address,ports,headers,times):
         self.address=address
         self.ports=ports
         self.headers=headers
+        self.times=times
     def scan(self):
-        h=httplib2.Http(timeout=2) #默认timeout=2 自己修改
+        h=httplib2.Http(timeout=self.times)
         try:
             for port in self.ports:
                 host="http://"+self.address+":"+port
                 print host
                 try:
                     res,con=h.request(host,"HEAD",headers=self.headers)
-                    print host+"----------------------------I GET IT"
+                    print host+"-------------I GET IT"
                 except Exception,e:
-                    #print e #错误信息
+                    #print e
                     pass
         except KeyboardInterrupt:
             print "Scan done\n"
@@ -37,5 +42,5 @@ class httpscan():
             print "Have a nice day"
 
 if __name__=="__main__":
-    test=httpscan(address,ports,headers)
+    test=httpscan(address,ports,headers,times)
     test.scan()
