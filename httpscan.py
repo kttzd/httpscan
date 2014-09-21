c9ffc5214df3920d52c79d474d426017bbbbb692
imlonghao <imlonghao@gmail.com>
2014-09-21 12:15:22 +0800

    Delete the useless space

--- httpscan.py
+++ httpscan.py
@@ -3,25 +3,16 @@
 #多端口请求http
 __author__ = 'kttzd'
 
-
 import sys
 import httplib2
-
-
 
 headers={}
 headers['User-Agent']="Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6"
 #headers['Referer']=""  #如果需要Referer从这里面加上
 
-
-
-
 address=sys.argv[1]
 
 ports=map(str,range(80,3000)) #默认80-2999 自己修改
-
-
-
 
 class httpscan():
     def __init__(self,address,ports,headers):
@@ -44,8 +35,7 @@ class httpscan():
             print "Scan done\n"
         finally:
             print "Have a nice day"
+
 if __name__=="__main__":
     test=httpscan(address,ports,headers)
     test.scan()
-                
-                
